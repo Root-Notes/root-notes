300bad5c38f4ce1547bdfeceae575b6f5495b7d0
Dax Harris <matteovh@gmail.com>
2023-09-25 08:39:07 -0400

    wip iconPicker

--- src/components/icons/IconPicker.tsx
+++ src/components/icons/IconPicker.tsx
@@ -1,6 +1,38 @@
-import { Paper, Stack, Text } from "@mantine/core";
-import { IconFamilies, IconRepresentation, iconTypes } from "./types";
-import { useMemo } from "react";
+import { Group, Modal, Paper, Stack, Text } from "@mantine/core";
+import {
+    IconFamilies,
+    IconFamilyNames,
+    IconRepresentation,
+    iconTypes,
+} from "./types";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { GenericIcon } from "./GenericIcon";
+import { startCase } from "lodash";
+
+function IconPickerModal({
+    open,
+    setOpen,
+    value,
+    setValue,
+    families,
+}: {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    value: IconRepresentation;
+    setValue: (value: IconRepresentation) => void;
+    families: IconFamilies[];
+}) {
+    const [tab, setTab] = useState<string | null>(families[0]);
+
+    return (
+        <Modal
+            className="icon-picker-modal"
+            opened={open}
+            onClose={() => setOpen(false)}
+            size="auto"
+        ></Modal>
+    );
+}
 
 export function IconPicker({
     // @ts-ignore
@@ -17,18 +49,79 @@ export function IconPicker({
 }) {
     const tabData: { family: IconFamilies; iconNames: string[] }[] = useMemo(
         () =>
-            Object.entries(iconTypes).map(([family, icons]) => ({
-                family: family as any,
-                iconNames: Object.keys(icons),
-            })),
+            Object.entries(iconTypes)
+                .filter(([family]) =>
+                    (families ?? ["md", "tb", "gi", "di"]).includes(
+                        family as any
+                    )
+                )
+                .map(([family, icons]) => ({
+                    family: family as any,
+                    iconNames: Object.keys(icons),
+                })),
         [families]
     );
-    console.log(tabData);
+
+    const [val, setVal] = useState<IconRepresentation>(
+        value ?? { family: "md", name: "MdDescription" }
+    );
+
+    useEffect(() => {
+        if (value) {
+            setVal(value);
+        }
+    }, [value?.family, value?.name]);
+
+    const setValue = useCallback(
+        (value: IconRepresentation) => {
+            if (onChange) {
+                onChange(value);
+            }
+            setVal(value);
+        },
+        [onChange]
+    );
+
+    const iconName = useMemo(() => {
+        const familyName = IconFamilyNames[val.family].name;
+        const iconName = startCase(val.name).split(" ", 2)[1];
+        return [familyName, iconName];
+    }, [val.family, val.name]);
+
+    const [open, setOpen] = useState(false);
 
     return (
-        <Stack gap="sm" className="custom-input icon-picker">
-            {label && <Text className="label">{label}</Text>}
-            <Paper withBorder p="sm" className="content"></Paper>
-        </Stack>
+        <>
+            <Stack gap="sm" className="custom-input icon-picker">
+                {label && <Text className="label">{label}</Text>}
+                <Paper
+                    p="sm"
+                    className="content"
+                    radius="sm"
+                    onClick={() => setOpen(true)}
+                >
+                    <Group gap="sm" justify="space-between">
+                        <GenericIcon
+                            family={val.family}
+                            name={val.name}
+                            size={24}
+                        />
+                        <Stack gap={0} className="icon-name" align="flex-end">
+                            <Text className="part name">{iconName[1]}</Text>
+                            <Text className="part family" size="xs" c="dimmed">
+                                {iconName[0]}
+                            </Text>
+                        </Stack>
+                    </Group>
+                </Paper>
+            </Stack>
+            <IconPickerModal
+                open={open}
+                setOpen={setOpen}
+                value={val}
+                setValue={setValue}
+                families={families ?? ["md", "tb", "gi", "di"]}
+            />
+        </>
     );
 }
