dd94a4f9902cd42f5c8b8217e8239c93be307fad
Dax Harris <matteovh@gmail.com>
2023-09-25 21:06:54 -0400

    Remove test iconpicker

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -2,7 +2,6 @@ import { Button, Paper, Stack } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 import { MdAdd, MdFolderOpen } from "react-icons/md";
 import AppLogo from "../../assets/icon.svg?react";
-import { IconPicker } from "@root-notes/common";
 
 export function HomePage() {
     const { t } = useTranslation();
@@ -16,7 +15,6 @@ export function HomePage() {
                 <Button leftSection={<MdFolderOpen />} fullWidth>
                     {t("views.home.buttons.open")}
                 </Button>
-                <IconPicker />
             </Stack>
         </Paper>
     );
